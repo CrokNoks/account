229cc015acf4409a88abd67cf22573b9fb87c61d
Lucas GUERRIER <l.guerrier@kutta.fr>
2026-03-08 20:24:45 +0100

    feat(frontend): restore SSR mode and re-enable server-side middleware

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,23 @@
+import { type NextRequest } from 'next/server'
+import { updateSession } from '@/shared/lib/supabase/middleware'
+import createMiddleware from 'next-intl/middleware';
+import {routing} from './i18n/routing';
+
+const intlMiddleware = createMiddleware(routing);
+
+export async function middleware(request: NextRequest) {
+  // 1. Run intl middleware
+  const response = intlMiddleware(request);
+
+  // 2. Update session (Supabase)
+  return await updateSession(request, response);
+}
+
+export const config = {
+  matcher: [
+    // Match all pathnames except for
+    // - … if they start with `/api`, `/_next` or `/_vercel`
+    // - … the ones containing a dot, e.g. `favicon.ico`
+    '/((?!api|_next|_vercel|.*\\..*).*)',
+  ],
+};
