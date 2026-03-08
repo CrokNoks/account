50140b0adf46e9cf983f17d6408e8fdec3d92fad
Lucas GUERRIER <l.guerrier@kutta.fr>
2026-03-08 09:28:16 +0100

    fix(backend): update port to 8080, enable CORS, and export AccountRepository

---
+++ functions/src/main.ts
@@ -0,0 +1,32 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Account V2 API')
+    .setDescription('The Account V2 API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
+  await app.listen(process.env.PORT ?? 8080);
+}
+bootstrap();
